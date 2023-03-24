912326979d64a34d157177496ece130f1dbaaaff
Pawan Kumar <pkprasad06@gmail.com>
2023-03-24 15:05:49 +0530

    use Reducer

--- src/user-management/AddUser.js
+++ src/user-management/AddUser.js
@@ -1,54 +1,83 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Button, Modal, Form, Alert } from "react-bootstrap";
 
-const AddUser = ({ onSave }) => {
 
-    console.log("Add User Re rendered.......");
-
-    const initialState = {
+const initialState = {
+    user: {
         fname: "",
         lname: "",
         email: "",
         phone: "",
         status: false
+    },
+    showModel : false
+}
+
+const addUserReducer = (state, action) =>{
+    const {type, payload } = action;
+
+    console.log("Action in Reducer ", action);
+    switch(type){
+        case "OPEN_MODEL":
+            return {
+                ...state,
+                showModel : true
+            }
+        case "CLOSE_MODEL":
+            return {
+                ...state,
+                showModel : false
+            }
+        case "INPUT_CHANGE":
+            return {
+                ...state,
+                 user: { ...state.user,  [payload.field]: payload.value }   
+            }
+        case "CHECKBOX_CHANGE":
+            return {
+                ...state,
+                user: { ...state.user,  [payload.field]: !state.user[payload.field]} 
+
+            }
+        case "FORM_SUBMITTED":
+            return {
+                ...initialState
+            }
+        default:
+            return {
+                ...state
+            }
     }
+}
 
-    const [show, setShow] = useState(false)
-    const [user, setUser] = useState(initialState)
 
-    const [error, setError] = useState(false)
+const AddUser = ({ onSave }) => {
 
-    // useEffect(()=>{
-    //     if(user.fname.length > 10){
-    //         setError(true);
-    //     }
-    //     return ()=>{
-    //         setError(false);
-    //     }
-    // },[user])
+   
+    const[state, dispatch] = useReducer(addUserReducer, initialState)
 
     //Handle Input Changes
     const handleInputChange = (e, field) => {
-        setUser((prevState) => ({ ...prevState, [field]: e.target.value }))
+        dispatch({ type: "INPUT_CHANGE", payload: { field, value: e.target.value }});
     }
 
     //Handle Checkbox Changes
     const handleCheckBoxChange = (e) => {
-        setUser((prevState) => ({ ...prevState, status: !prevState.status }))
+        dispatch({ type: "CHECKBOX_CHANGE", payload: {field:'status'}});
     }
 
     //Handle OnSubmit button
     const handleOnSubmit = () => {
-        onSave(user); //Send Data to parent component
-        setShow(false) // Hide Model Popup
-        setUser(initialState) // Reset state value to clear form
+        onSave(state.user); //Send Data to parent component
+        dispatch({ type: "FORM_SUBMITTED"});
+        
     }
 
 
     return (
         <div className="text-end addComponent" style={{ marginBottom: "10px" }}>
-            <Button onClick={() => setShow(true)}>Add User</Button>
-            <Modal show={show} onHide={() => setShow(false)}
+            <Button onClick={() =>  dispatch({type:"OPEN_MODEL"})}>Add User</Button>
+            <Modal show={ state.showModel} onHide={() => dispatch({type:"CLOSE_MODEL"})}
                 size="lg"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
@@ -59,34 +88,30 @@ const AddUser = ({ onSave }) => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    {error && <Alert variant='danger'>
-                    Some of the inputs are not valid
-                </Alert>}
-                
                     <Form>
                         <Form.Group className="mb-3">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" value={user.fname} placeholder="Enter First Name" onChange={(e) => handleInputChange(e, 'fname')} autoFocus />
+                            <Form.Control type="text" value={state.user.fname} placeholder="Enter First Name" onChange={(e) => handleInputChange(e, 'fname')} autoFocus />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" value={user.lname} placeholder="Enter Last Name" onChange={(e) => handleInputChange(e, 'lname')} />
+                            <Form.Control type="text" value={state.user.lname} placeholder="Enter Last Name" onChange={(e) => handleInputChange(e, 'lname')} />
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" value={user.email} placeholder="Enter Email" onChange={(e) => handleInputChange(e, 'email')} />
+                            <Form.Control type="email" value={state.user.email} placeholder="Enter Email" onChange={(e) => handleInputChange(e, 'email')} />
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Form.Label>Phone</Form.Label>
-                            <Form.Control type="text" value={user.phone} placeholder="Enter Phone Number" onChange={(e) => handleInputChange(e, 'phone')} />
+                            <Form.Control type="text" value={state.user.phone} placeholder="Enter Phone Number" onChange={(e) => handleInputChange(e, 'phone')} />
                         </Form.Group>
                         <Form.Group className="mb-3">
-                            <Form.Check type="checkbox" checked={user.status} label="IsActive" onChange={handleCheckBoxChange} />
+                            <Form.Check type="checkbox" checked={state.user.status} label="IsActive" onChange={handleCheckBoxChange} />
                         </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="default" onClick={() => setShow(false)} >Close</Button>
+                    <Button variant="default" onClick={() => dispatch({type:"CLOSE_MODEL"})} >Close</Button>
                     <Button variant="primary" type="submit" onClick={handleOnSubmit}>
                         Create
                     </Button>
